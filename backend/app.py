ef2db3026389ae7dce9731eeab667094cdb48ae1
Akhmadjon Rajabov <ahmadjon_97_97@mail.ru>
2026-02-22 12:23:54 +0100

    Backed, Nginx and docker-compsoe setup

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b"Hello from Effective Mobile!!!")
+    
+if __name__ == '__main__':
+    server_address = ('0.0.0.0', 8080)
+    httpd = HTTPServer(server_address, SimpleHandler)
+    print("Starting backend server on port 8080...")
+    httpd.serve_forever()
